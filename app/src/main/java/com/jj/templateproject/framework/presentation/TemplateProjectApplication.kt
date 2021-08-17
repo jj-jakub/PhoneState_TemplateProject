eedc28ac3b874e7a783691267e0b0bc3ef4dafd0
jj <>
2021-08-17 18:27:21 +0200

    Extract KoinLauncher

--- app/src/main/java/com/jj/templateproject/framework/presentation/TemplateProjectApplication.kt
+++ app/src/main/java/com/jj/templateproject/framework/presentation/TemplateProjectApplication.kt
@@ -1,21 +1,17 @@
 package com.jj.templateproject.framework.presentation
 
 import android.app.Application
+import com.jj.templateproject.di.koin.KoinLauncher
 import com.jj.templateproject.di.koin.mainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class TemplateProjectApplication : Application() {
 
+    private val koinLauncher = KoinLauncher()
+
     override fun onCreate() {
         super.onCreate()
-        launchKoin()
-    }
-
-    private fun launchKoin() {
-        startKoin {
-            androidContext(this@TemplateProjectApplication)
-            modules(mainModule)
-        }
+        koinLauncher.startKoin(this)
     }
 }
