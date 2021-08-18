ae78f58a33ff8ec27b82baa5e9581df069b5652f
jj <unknown>
2021-08-18 14:04:12 +0200

    Manage AirplaneModeState in DeviceStateManager

--- app/src/main/java/com/jj/templateproject/data/device/DeviceStateManager.kt
+++ app/src/main/java/com/jj/templateproject/data/device/DeviceStateManager.kt
@@ -1,6 +1,8 @@
 package com.jj.templateproject.data.device
 
 import com.jj.templateproject.data.coroutines.ICoroutineScopeProvider
+import com.jj.templateproject.domain.airplanemode.AirplaneModeManager
+import com.jj.templateproject.domain.airplanemode.AirplaneModeState
 import com.jj.templateproject.domain.device.IDeviceStateManager
 import com.jj.templateproject.domain.network.NetworkManager
 import com.jj.templateproject.domain.network.NetworkState
@@ -11,13 +13,15 @@ import kotlinx.coroutines.launch
 
 data class DeviceState(
     val networkState: NetworkState = NetworkState.Unknown,
+    val airplaneModeState: AirplaneModeState = AirplaneModeState.Unknown,
     val change: DeviceStateChange = DeviceStateChange.NONE
 )
 
-enum class DeviceStateChange { NETWORK, NONE }
+enum class DeviceStateChange { NETWORK, AIRPLANE, NONE }
 
 class DeviceStateManager(
     networkManager: NetworkManager,
+    airplaneModeManager: AirplaneModeManager,
     coroutineScopeProvider: ICoroutineScopeProvider
 ) : IDeviceStateManager {
 
@@ -29,12 +33,25 @@ class DeviceStateManager(
         coroutineScopeProvider.createIOScope().launch {
             networkManager.observeNetworkState().collect { onNetworkStateChanged(it) }
         }
+
+        coroutineScopeProvider.createIOScope().launch {
+            airplaneModeManager.observeAirplaneModeState()
+                .collect { onAirplaneModeStateChanged(it) }
+        }
     }
 
     private fun onNetworkStateChanged(networkState: NetworkState) {
         val newDeviceState = deviceStateFlow.value.copy(
             networkState = networkState,
             change = DeviceStateChange.NETWORK
+        )
+        changeDeviceStateFlow(newDeviceState)
+    }
+
+    private fun onAirplaneModeStateChanged(airplaneModeState: AirplaneModeState) {
+        val newDeviceState = deviceStateFlow.value.copy(
+            airplaneModeState = airplaneModeState,
+            change = DeviceStateChange.AIRPLANE
         )
         changeDeviceStateFlow(newDeviceState)
     }
