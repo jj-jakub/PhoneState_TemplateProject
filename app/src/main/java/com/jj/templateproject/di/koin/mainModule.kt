95f2b486ae1434eab43f1022cb51604d533b54cf
jj <>
2021-08-18 09:49:44 +0200

    Extract VersionTextProvider

--- app/src/main/java/com/jj/templateproject/di/koin/mainModule.kt
+++ app/src/main/java/com/jj/templateproject/di/koin/mainModule.kt
@@ -3,6 +3,7 @@ package com.jj.templateproject.di.koin
 import com.jj.templateproject.data.coroutines.CoroutineScopeProvider
 import com.jj.templateproject.data.coroutines.ICoroutineScopeProvider
 import com.jj.templateproject.data.network.RetrofitFactory
+import com.jj.templateproject.data.text.VersionTextProvider
 import com.jj.templateproject.domain.network.NetworkManager
 import com.jj.templateproject.framework.network.AndroidNetworkManager
 import com.jj.templateproject.framework.network.NetworkStateListener
@@ -14,4 +15,9 @@ val mainModule = module {
     single <ICoroutineScopeProvider> { CoroutineScopeProvider() }
     single { NetworkStateListener() }
     single <NetworkManager> { AndroidNetworkManager(get(), get(), get()) }
+}
+
+val textModule = module {
+
+    single { VersionTextProvider() }
 }
