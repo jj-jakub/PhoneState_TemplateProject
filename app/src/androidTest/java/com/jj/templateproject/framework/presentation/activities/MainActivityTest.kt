a523ab5a635cae6b97156feee34f96113f65ea03
jj <>
2021-08-26 10:54:29 +0200

    Fix existing UI test

--- app/src/androidTest/java/com/jj/templateproject/framework/presentation/activities/MainActivityTest.kt
+++ app/src/androidTest/java/com/jj/templateproject/framework/presentation/activities/MainActivityTest.kt
@@ -7,11 +7,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.UiDevice
 import com.jj.templateproject.R
-import com.jj.templateproject.data.utils.getAboutVersionText
-import org.junit.Before
+import com.jj.templateproject.data.text.VersionTextProvider
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,15 +16,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    private lateinit var mDevice: UiDevice
-
     @get:Rule
     var rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
-
-    @Before
-    fun setup() {
-        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-    }
 
     @Test
     fun mainLayoutShouldBeVisible() {
@@ -36,7 +26,7 @@ class MainActivityTest {
 
     @Test
     fun shouldShowProperInfoInMainLabel() {
-        val expectedText = getAboutVersionText()
+        val expectedText = VersionTextProvider().getAboutVersionText()
         onView(withId(R.id.mainLabel)).check(matches(withText(expectedText)))
     }
 }
