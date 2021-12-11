a95e28af26e8df7a22d677bb031cfaa311c542c7
Jakub Jasiński <jj.jakub.jasinski@gmail.com>
2021-12-11 12:09:21 +0100

    Create tests for StatesViewTypes color getter

---
+++ app/src/test/java/com/jj/templateproject/framework/presentation/adapters/statelistitems/StatesViewTypesTest.kt
@@ -0,0 +1,44 @@
+package com.jj.templateproject.framework.presentation.adapters.statelistitems
+
+import com.jj.templateproject.R
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class StatesViewTypesTest {
+
+    @Test
+    fun `should return red color resource for Type1`() {
+        val statesViewType = StatesViewTypes.Type1
+
+        val colorRes = statesViewType.color
+
+        assertEquals(R.color.red, colorRes)
+    }
+
+    @Test
+    fun `should return yellow color resource for Type2`() {
+        val statesViewType = StatesViewTypes.Type2
+
+        val colorRes = statesViewType.color
+
+        assertEquals(R.color.yellow, colorRes)
+    }
+
+    @Test
+    fun `should return blue color resource for Type3`() {
+        val statesViewType = StatesViewTypes.Type3
+
+        val colorRes = statesViewType.color
+
+        assertEquals(R.color.blue, colorRes)
+    }
+
+    @Test
+    fun `should return green color resource for Type4`() {
+        val statesViewType = StatesViewTypes.Type4
+
+        val colorRes = statesViewType.color
+
+        assertEquals(R.color.green, colorRes)
+    }
+}
