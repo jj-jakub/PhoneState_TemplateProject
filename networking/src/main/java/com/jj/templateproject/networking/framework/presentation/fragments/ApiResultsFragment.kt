90e87cf478b8b907c1f6fc79ffda8c4c48e4cb54
Jakub Jasiński <jj.jakub.jasinski@gmail.com>
2021-12-11 14:26:18 +0100

    Create Networking library module

---
+++ networking/src/main/java/com/jj/templateproject/networking/framework/presentation/fragments/ApiResultsFragment.kt
@@ -0,0 +1,11 @@
+package com.jj.templateproject.networking.framework.presentation.fragments
+
+import com.jj.templateproject.framework.presentation.fragments.BaseFragment
+import com.jj.templateproject.networking.R
+
+class ApiResultsFragment: BaseFragment(R.layout.fragment_api_results) {
+
+    override fun setupSubscriptions() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ networking/src/main/java/com/jj/templateproject/networking/framework/viewmodels/ApiResultsViewModel.kt
@@ -0,0 +1,8 @@
+package com.jj.templateproject.networking.framework.viewmodels
+
+import com.jj.templateproject.framework.viewmodels.BaseViewModel
+
+class ApiResultsViewModel: BaseViewModel() {
+
+
+}
