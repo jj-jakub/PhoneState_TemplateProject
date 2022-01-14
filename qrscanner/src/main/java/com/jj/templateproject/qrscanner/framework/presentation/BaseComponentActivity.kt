f11c059fb674b82f977fdd6d035893d0e23e507d
Jakub Jasiński <jj.jakub.jasinski@gmail.com>
2022-01-14 16:20:17 +0100

    Move QrScanner from activity to fragment

--- qrscanner/src/main/java/com/jj/templateproject/qrscanner/framework/presentation/BaseComponentActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.jj.templateproject.qrscanner.framework.presentation
-
-import androidx.activity.ComponentActivity
-
-open class BaseComponentActivity: ComponentActivity()
---
+++ qrscanner/src/main/java/com/jj/templateproject/qrscanner/framework/presentation/QrScannerFragment.kt
@@ -0,0 +1,124 @@
+package com.jj.templateproject.qrscanner.framework.presentation
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Size
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnLifecycleDestroyed
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.jj.templateproject.core.framework.presentation.fragments.BaseFragment
+import com.jj.templateproject.qrscanner.R
+import com.jj.templateproject.qrscanner.data.QrCodeAnalyzer
+import com.jj.templateproject.qrscanner.framework.presentation.theme.ComposeQrScannerTheme
+
+class QrScannerFragment : BaseFragment(R.layout.fragment_qr_scanner) {
+
+    override fun setupSubscriptions() {
+        /* no-op */
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(DisposeOnLifecycleDestroyed(viewLifecycleOwner))
+            setContent {
+                ComposeQrScannerTheme {
+                    var discoveredQrCode by remember {
+                        mutableStateOf("")
+                    }
+                    val context = LocalContext.current
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    val cameraProviderFuture = remember {
+                        ProcessCameraProvider.getInstance(context)
+                    }
+                    var hasCameraPermission by remember {
+                        mutableStateOf(
+                                ContextCompat.checkSelfPermission(
+                                        context, Manifest.permission.CAMERA
+                                ) == PackageManager.PERMISSION_GRANTED
+                        )
+                    }
+                    val launcher = rememberLauncherForActivityResult(
+                            contract = ActivityResultContracts.RequestPermission(),
+                            onResult = { granted ->
+                                hasCameraPermission = granted
+                            }
+                    )
+                    LaunchedEffect(key1 = true) {
+                        launcher.launch(Manifest.permission.CAMERA)
+                    }
+                    Column(
+                            modifier = Modifier.fillMaxSize()
+                    ) {
+                        if (hasCameraPermission) {
+                            AndroidView(factory = { context ->
+                                val previewView = PreviewView(context)
+                                val preview = Preview.Builder().build()
+                                val selector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+                                preview.setSurfaceProvider(previewView.surfaceProvider)
+                                val imageAnalysis = ImageAnalysis.Builder()
+                                    .setTargetResolution(Size(previewView.width, previewView.height))
+                                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                    .build()
+                                imageAnalysis.setAnalyzer(
+                                        ContextCompat.getMainExecutor(context),
+                                        QrCodeAnalyzer { result ->
+                                            discoveredQrCode = result
+                                        }
+                                )
+
+                                try {
+                                    cameraProviderFuture.get().bindToLifecycle(
+                                            lifecycleOwner,
+                                            selector,
+                                            preview,
+                                            imageAnalysis)
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
+                                previewView
+                            },
+                                    modifier = Modifier.weight(1f))
+                            Text(
+                                    text = discoveredQrCode,
+                                    color = Color.Red,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(32.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+}
