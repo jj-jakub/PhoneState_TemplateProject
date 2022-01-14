f4542674d4f7eff2afa6c201095323f72531ca11
Jakub Jasiński <jj.jakub.jasinski@gmail.com>
2022-01-14 17:04:36 +0100

    Scan for text using mlkit-text-recognition

--- qrscanner/src/main/java/com/jj/templateproject/qrscanner/data/QrCodeAnalyzer.kt
+++ qrscanner/src/main/java/com/jj/templateproject/qrscanner/data/QrCodeAnalyzer.kt
@@ -9,6 +9,8 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class QrCodeAnalyzer(private val onQrCodeScanned: (String) -> Unit) : ImageAnalysis.Analyzer {
 
@@ -21,17 +23,21 @@ class QrCodeAnalyzer(private val onQrCodeScanned: (String) -> Unit) : ImageAnaly
     private val options = BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build()
 
     private val scanner = BarcodeScanning.getClient(options)
+    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
     @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(image: ImageProxy) {
         if (image.format in supportedImageFormats && image.image != null) {
-            val qrImage = InputImage.fromMediaImage(image.image, image.imageInfo.rotationDegrees)
+            val inputImage = InputImage.fromMediaImage(image.image, image.imageInfo.rotationDegrees)
 
-            scanner.process(qrImage).addOnSuccessListener {
-                if (it.isNotEmpty()) {
-                    val qrInfo = it.first().rawValue
-                    onQrCodeScanned(qrInfo)
-                }
+            recognizer.process(inputImage).addOnSuccessListener {
+//                if (it.isNotEmpty()) {
+//                    val qrInfo = it.first().rawValue
+//                    Log.d("ABAB", "discovered: $qrInfo")
+//                    onQrCodeScanned(qrInfo)
+//                }
+                Log.d("ABAB", "discovered: ${it.text}")
+                onQrCodeScanned(it.text)
                 image.close()
             }.addOnFailureListener {
                 Log.e(QrCodeAnalyzer::class.java.simpleName, "Scanning error", it)
