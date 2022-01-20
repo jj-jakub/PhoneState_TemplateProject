6082f251b03310b946e9493864ffde2bd06b2224
Jakub Jasiński <jj.jakub.jasinski@gmail.com>
2022-01-20 11:22:43 +0100

    Implement AddEditNoteViewModel

--- notes/src/main/java/com/jj/templateproject/notes/featureaddeditnote/presentation/viewmodels/AddEditNoteViewModel.kt
+++ notes/src/main/java/com/jj/templateproject/notes/featureaddeditnote/presentation/viewmodels/AddEditNoteViewModel.kt
@@ -1,5 +1,6 @@
 package com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels
 
+import androidx.lifecycle.viewModelScope
 import com.jj.templateproject.core.framework.presentation.viewmodels.BaseViewAction
 import com.jj.templateproject.core.framework.presentation.viewmodels.BaseViewModel
 import com.jj.templateproject.core.framework.presentation.viewmodels.states.BaseViewState
@@ -13,15 +14,19 @@ import com.jj.templateproject.notes.featureaddeditnote.presentation.utils.AddEdi
 import com.jj.templateproject.notes.featureaddeditnote.presentation.utils.AddEditNoteViewEvent.EnteredTitleCharacter
 import com.jj.templateproject.notes.featureaddeditnote.presentation.utils.AddEditNoteViewEvent.SaveNote
 import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewAction
+import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewAction.NoteColorChanged
 import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewAction.NoteContentChanged
 import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewAction.NoteContentFieldFocusChanged
 import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewAction.NoteTitleChanged
 import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewAction.NoteTitleFieldFocusChanged
 import com.jj.templateproject.notes.featureaddeditnote.presentation.viewmodels.AddEditNoteViewModel.ViewState
+import com.jj.templateproject.notes.featurenote.domain.exceptions.InvalidNoteException
 import com.jj.templateproject.notes.featurenote.domain.model.Note
 import com.jj.templateproject.notes.featurenote.domain.usecases.NoteUseCases
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class AddEditNoteViewModel(private val useCases: NoteUseCases) : BaseViewModel<ViewState, ViewAction>(ViewState()) {
 
@@ -30,8 +35,8 @@ class AddEditNoteViewModel(private val useCases: NoteUseCases) : BaseViewModel<V
             val currentlySelectedColor: Int = Note.noteColors.random(),
             val noteTitle: String = "",
             val noteContent: String = "",
-            val noteTitleState: AddEditNoteTextFieldState = AddEditNoteTextFieldState(),
-            val noteContentState: AddEditNoteTextFieldState = AddEditNoteTextFieldState(),
+            val noteTitleState: AddEditNoteTextFieldState = AddEditNoteTextFieldState(hint = "Enter title"),
+            val noteContentState: AddEditNoteTextFieldState = AddEditNoteTextFieldState(hint = "Enter content"),
     ) : BaseViewState
 
     sealed class ViewAction : BaseViewAction {
@@ -39,6 +44,7 @@ class AddEditNoteViewModel(private val useCases: NoteUseCases) : BaseViewModel<V
         class NoteContentChanged(val currentContent: String) : ViewAction()
         class NoteTitleFieldFocusChanged(val hasFocus: Boolean) : ViewAction()
         class NoteContentFieldFocusChanged(val hasFocus: Boolean) : ViewAction()
+        class NoteColorChanged(val color: Int) : ViewAction()
     }
 
     override fun reduceState(viewAction: ViewAction): ViewState =
@@ -47,12 +53,15 @@ class AddEditNoteViewModel(private val useCases: NoteUseCases) : BaseViewModel<V
             is NoteContentChanged -> state.copy(noteTitleState = state.noteContentState.copy(content = viewAction.currentContent))
             is NoteTitleFieldFocusChanged -> state.copy(noteTitleState = state.noteTitleState.copy(
                     isHintVisible = viewAction.hasFocus && state.noteTitleState.content.isEmpty()))
-            is NoteContentFieldFocusChanged ->  state.copy(noteTitleState = state.noteContentState.copy(
+            is NoteContentFieldFocusChanged -> state.copy(noteTitleState = state.noteContentState.copy(
                     isHintVisible = viewAction.hasFocus && state.noteContentState.content.isEmpty()))
+            is NoteColorChanged -> state.copy(currentlySelectedColor = viewAction.color)
         }
 
     private val mutableAddEditNoteUIEffects = MutableSharedFlow<AddEditNoteUIEffect>()
     val addEditNoteUIEffects = mutableAddEditNoteUIEffects.asSharedFlow()
+
+    private var currentNoteId: Int? = null
 
     fun onEvent(event: AddEditNoteViewEvent) {
         when (event) {
@@ -60,9 +69,27 @@ class AddEditNoteViewModel(private val useCases: NoteUseCases) : BaseViewModel<V
             is EnteredContentCharacter -> sendViewAction(NoteTitleChanged(event.currentContent))
             is ChangeTitleFocus -> sendViewAction(NoteTitleFieldFocusChanged(event.hasFocus))
             is ChangeContentFocus -> sendViewAction(NoteContentFieldFocusChanged(event.hasFocus))
-            is ChangeColor -> TODO()
-            SaveNote -> TODO()
+            is ChangeColor -> sendViewAction(NoteColorChanged(event.color))
+            SaveNote -> saveNote()
         }
     }
 
+    private fun saveNote() {
+        viewModelScope.launch {
+            try {
+                useCases.addNoteUseCase(
+                        Note(
+                                title = state.noteTitle,
+                                content = state.noteContent,
+                                timestamp = Date().time,
+                                color = state.currentlySelectedColor,
+                                id = currentNoteId
+                        )
+                )
+                mutableAddEditNoteUIEffects.emit(AddEditNoteUIEffect.SaveNote)
+            } catch (e: InvalidNoteException) {
+                mutableAddEditNoteUIEffects.emit(AddEditNoteUIEffect.ShowToast(message = e.localizedMessage ?: "Can't save note"))
+            }
+        }
+    }
 }
